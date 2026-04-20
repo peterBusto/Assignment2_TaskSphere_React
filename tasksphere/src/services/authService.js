1d42e9453d7a63fa8730f0b492a91a0319f03f02
Peter <pebu001@ess.ais.ac.nz>
2026-04-20 22:30:45 +1200

    Fixing the issue in Vercel

--- tasksphere/src/services/authService.js
+++ tasksphere/src/services/authService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api/auth';
+const API_BASE_URL = 'https://assignment2-task-sphere-django.vercel.app/api/auth';
 
 export const authService = {
   // Registration
